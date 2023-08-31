cbf17283c13a3bdb0d88ad73949a4dde4cd9b883
mariiapelypyshyn <maria.peleshko97@gmail.com>
2023-08-31 18:31:37 +0300

    homework 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+console.log(galleryList);
+
+const renderList = (arr, container) => {
+    const markup = arr.map(
+        (item) => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`).join("");
+
+    container.insertAdjacentHTML("afterbegin", markup);
+};
+
+renderList(galleryItems, galleryList);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
+galleryList.style.listStyle = "none";
